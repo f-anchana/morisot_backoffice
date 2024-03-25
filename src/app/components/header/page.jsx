82f0365aae08b0ccd8c0f0828e7333d631f29ebd
Anchana <fatimarajananchana@gmail.com>
2024-03-25 09:36:29 +0100

    test1

---
+++ src/app/components/header/page.jsx
@@ -0,0 +1,8 @@
+'use client';
+
+
+export const Header = () => {
+    return <h2>zerrz</h2>
+
+    };
+    export default Header;
---
+++ src/app/page.jsx
@@ -0,0 +1,15 @@
+'use client';
+
+
+import Header from "./components/header/page";
+
+export default function Home() {
+  return (
+    <main>
+      <Header /> 
+  <h1>heyyy</h1>
+  <p>franchement on va s'en sortir les gars</p>
+    </main>
+  );
+}
+
